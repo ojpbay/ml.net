c7be6ac053a821294e3226440029bdebc334081f
Oliver Baylis <oli.baylis@gmail.com>
2019-05-25 08:49:16 +0100

    From https://dotnet.microsoft.com/learn/machinelearning-ai/ml-dotnet-get-started-tutorial/consume

---
+++ ml.net/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ML;
+using Ml.netML.Model.DataModels;
+using System;
+
+namespace ml.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumeModel();
+        }
+
+        public static void ConsumeModel()
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Use the code below to add input data
+            var input = new ModelInput();
+            input.SentimentText = "that looks good";
+
+            // Try model on sample data
+            // True is toxic, false is non-toxic
+            ModelOutput result = predEngine.Predict(input);
+
+            Console.WriteLine($"Text: {input.SentimentText} | Prediction: {(Convert.ToBoolean(result.Prediction) ? "Toxic" : "Non Toxic")} sentiment");
+        }
+    }
+}
